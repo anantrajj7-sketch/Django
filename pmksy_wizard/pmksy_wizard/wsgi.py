ed63c440a2e321debd2333444f3e0f88b3f170d4
anantrajj7-sketch <anantrajj7@gmail.com>
2025-09-20 15:41:26 +0530

    Delete pmksy_wizard directory

--- pmksy_wizard/pmksy_wizard/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""WSGI config for pmksy_wizard project."""
-from __future__ import annotations
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pmksy_wizard.settings")
-
-application = get_wsgi_application()
